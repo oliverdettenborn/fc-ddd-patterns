771089fafddb03ad4ef23f719718735ac98f31bf
oliverdettenborn <oliver.dettenborn@gmail.com>
2024-08-13 20:41:07 -0300

    add type on event handler

--- src/domain/customer/event/handler/send-message-when-customer-change-address.handler.ts
+++ src/domain/customer/event/handler/send-message-when-customer-change-address.handler.ts
@@ -1,7 +1,7 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
 import CustomerChangeAddressEvent from "../customer-change-address.event";
 
-export default class SendMessageWhenCustomerChangeAddressHandler implements EventHandlerInterface {
+export default class SendMessageWhenCustomerChangeAddressHandler implements EventHandlerInterface<CustomerChangeAddressEvent> {
     handle(event: CustomerChangeAddressEvent): void {
         const {id, name, Address} = event.eventData;
 
--- src/domain/customer/event/handler/send-second-log-when-customer-is.created.handler.ts
+++ src/domain/customer/event/handler/send-second-log-when-customer-is.created.handler.ts
@@ -1,7 +1,7 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
 import CustomerCreatedEvent from "../customer-created.event";
 
-export default class SendSecondLogWhenCustomerIsCreatedHandler implements EventHandlerInterface {
+export default class SendSecondLogWhenCustomerIsCreatedHandler implements EventHandlerInterface<CustomerCreatedEvent> {
     handle(event: CustomerCreatedEvent): void {
         console.log("Esse é o segundo console.log do evento: CustomerCreated")
     }
